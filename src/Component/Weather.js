827acf69078f698949526296ae0417782529919f
Hritik Choudhary <hritikchoudhary1000@gmail.com>
2021-03-11 14:04:33 +0530

    reverted

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -6,7 +6,7 @@ const Weather = () => {
         <div className="Weather-container">
             <div className="weather-content">
                 <Searchbar/>
-                <Searchbar/>
+                
             </div>
         </div>
     )
